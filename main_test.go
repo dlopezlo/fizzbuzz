cb0ef35e96e77b1887d0b75ec0f0b937a6a7cd0b
Daniel López <dlopezlo@gmail.com>
2021-07-27 19:13:51 +0200

    add names to tests

--- main_test.go
+++ main_test.go
@@ -4,28 +4,23 @@ import "testing"
 
 func TestFizzBuzz(t *testing.T) {
 	tt := []struct {
+		name   string
 		number uint64
 		result string
 	}{
-		{1, "1"},
-		{2, "2"},
-		{3, "Fizz"},
-		{4, "4"},
-		{5, "Buzz"},
-		{9, "Fizz"},
-		{10, "Buzz"},
-		{15, "FizzBuzz"},
-		{30, "FizzBuzz"},
-		{50, "Buzz"},
-		{55, "Buzz"},
-		{500, "Buzz"},
-		{933, "Fizz"},
+		{"a number should return the same number", 1, "1"},
+		{"3 should return Fizz", 3, "Fizz"},
+		{"5 should return Buzz", 5, "Buzz"},
+		{"a multiple of 3 should return Fizz", 9, "Fizz"},
+		{"a multiple of 5 should return Buzz", 10, "Buzz"},
+		{"A multiple of both 3 and five should return FizzBuzz", 15, "FizzBuzz"},
 	}
 
 	for _, tc := range tt {
 		r := fibo(tc.number)
 		if r != tc.result {
-			t.Errorf("fibo(%v) got %v and should be %v", tc.number, r, tc.result)
+			t.Errorf("[Test name: %v] fibo(%v) got %v and should be %v", tc.name, tc.number, r, tc.result)
+
 		}
 	}
 }
